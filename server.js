ae81df0cd0a18ac2dcc088146cb0df16609ef66d
Moeed Azam <Moeedazam2000@gmail.com>
2025-06-25 00:24:47 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { Client } = require("linkedin-private-api");
+
+const app = express();
+app.use(express.json());
+
+app.post("/send-linkedin-message", async (req, res) => {
+  const { li_at_cookie, recipientProfileId, message } = req.body;
+
+  try {
+    const client = new Client();
+    await client.login.userCookie({ li_at: li_at_cookie });
+
+    await client.message.sendText(recipientProfileId, message);
+
+    res.json({ success: true });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
